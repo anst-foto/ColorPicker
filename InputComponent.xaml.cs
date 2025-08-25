79d4341a3fe4c2e989cb6a7c0aa2c3aaad73b34e
Starinin Andrey <starinin-andrey@ya.ru>
2025-08-25 21:11:04 +0300

    Initial commit

---
+++ InputComponent.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorPicker;
+
+public partial class InputComponent : UserControl
+{
+    public string LabelText { get; set; }
+    public event Action<double> Action;
+    
+    public InputComponent()
+    {
+        InitializeComponent();
+        
+        Loaded += (sender, args) => Label_Input.Content = LabelText;
+    }
+
+    private void Input_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        Action.Invoke(e.NewValue);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ColorPicker;
+
+public partial class MainWindow : Window
+{
+    private byte _alpha;
+    private byte _red;
+    private byte _green;
+    private byte _blue;
+    
+    public MainWindow()
+    {
+        _alpha = 255;
+        _red = 0;
+        _green = 0;
+        _blue = 0;
+        
+        InitializeComponent();
+    }
+    
+    private void Alpha_OnAction(double value)
+    {
+        _alpha = (byte)value;
+        FillColor();
+    }
+    
+    private void Red_OnAction(double value)
+    {
+        _red = (byte)value;
+        FillColor();
+    }
+    private void Green_OnAction(double value)
+    {
+        _green = (byte)value;
+        FillColor();
+    }
+    private void Blue_OnAction(double value)
+    {
+        _blue = (byte)value;
+        FillColor();
+    }
+
+    private void FillColor()
+    {
+        var color = Color.FromArgb(_alpha, _red, _green, _blue);
+        Rectangle.Fill = new SolidColorBrush(color);
+        
+        RGBA.Text = $"RGBA: {_red}, {_green}, {_blue}, {_alpha}";
+        HEX.Text = $"HEX: #{_red:X2}{_green:X2}{_blue:X2}{_alpha:X2}";
+    }
+}
