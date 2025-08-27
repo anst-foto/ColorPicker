7f7f0e175a67c66b1eab5d19e816a46e508d1255
Starinin Andrey <starinin-andrey@ya.ru>
2025-08-27 21:11:05 +0300

    Добавление Binding'ов

---
+++ DoubleToByteConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ColorPicker;
+
+public class DoubleToByteConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (targetType != typeof(byte)) return DependencyProperty.UnsetValue;
+        
+        var val = byte.Parse(value.ToString());
+        return val;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return DependencyProperty.UnsetValue;
+    }
+}
---
+++ DoubleToHexConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ColorPicker;
+
+public class DoubleToHexConverter : IMultiValueConverter
+{
+    private static string Convert(object value, Type targetType)
+    {
+        var val = int.Parse(value.ToString());
+        return targetType != typeof(string) 
+            ? string.Empty
+            : $"{val:X2}";
+    }
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var result = "#";
+        foreach (var value in values)
+        {
+            result += Convert(value, targetType);
+        }
+        
+        return result;
+        
+        // return values.Aggregate("#", (current, value) => current + Convert(value, targetType));
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return [DependencyProperty.UnsetValue];
+    }
+}
